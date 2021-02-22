560ae13fe9cb8788b3544094fb96d2b67acb5840
MAC <andrejkrupoves@gmail.com>
2021-02-23 07:37:43 +1100

    basic OOP

--- object.js
+++ object.js
@@ -1,6 +1,10 @@
 let dog = {
     name:"Roy",
-    numLegs:4
+    numLegs:4,
+    sayLegs: function(){
+        return (`This dog has ${dog.numLegs} legs`);
+    }
 }
 
-console.log(dog.name)
+console.log(dog.name);
+console.log(dog.sayLegs());
