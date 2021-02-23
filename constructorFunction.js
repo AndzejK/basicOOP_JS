41af3489c49446dd9068d3396bf0f6f88cd31619
MAC <andrejkrupoves@gmail.com>
2021-02-24 06:07:35 +1100

    just theory

--- constructorFunction.js
+++ constructorFunction.js
@@ -31,3 +31,40 @@ function Dogparam(name,colour){
 
 let terrier=new Dogparam("Copacetic","white");
 console.log(terrier);
+
+// ## Verify an Object's Constructor with instanceof
+
+function House(numBedrooms) {
+    this.numberOfBedrooms=numBedrooms
+}
+
+let myHouse = new House(4);
+let newHouse= new House(5);
+console.log(myHouse instanceof House);
+
+// ### Understand Own Properties
+/*
+    numberOfBedrooms is called own property, it can be as much as you want,
+    since it's defined directly on the instance object.
+    Secondly, myHouse and New House each has its own separate COPY of this
+    property. In fact every instance of House WILL have its own copy
+    of this property.
+*/
+
+let ownProps=[];
+// Here we're looping through Object,syntex.
+for(let property_can_be_anythingHere in myHouse){
+    if (myHouse.hasOwnProperty(property_can_be_anythingHere)){
+        ownProps.push(property_can_be_anythingHere)
+    }
+}
+console.log(ownProps);
+
+// ####  prototype Property
+/*
+    The prototype property allows you to add properties and methods to an object.
+    => Syntax
+    object.prototype.name = value
+*/
+House.prototype.numBaths=2;
+console.log(newHouse.numBaths);
